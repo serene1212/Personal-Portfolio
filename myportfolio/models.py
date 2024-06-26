5d49f21437446754d6e591c3340904427e212d9f
arshia <arshiarezagholi1212@gmail.com>
2024-06-26 21:52:34 +0330

    removed dates from course model

--- myportfolio/models.py
+++ myportfolio/models.py
@@ -42,8 +42,6 @@ class Education(models.Model):
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
-    start_date = models.DateField()
-    end_date = models.DateField()
     certificate = models.URLField(default=None, blank=True, null=True)
 
 
