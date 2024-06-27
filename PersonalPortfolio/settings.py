80fdbde55c014dc218dbc1fdf198f1bba255646d
arshia_rezagholi <146835122+serene1212@users.noreply.github.com>
2024-06-27 18:35:40 +0330

    Update settings.py

--- PersonalPortfolio/settings.py
+++ PersonalPortfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wvkz8dknd4g!6y)$ma15m*y)865gj8t48kiyu0f=sqtpr&wej6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'arshio12.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
